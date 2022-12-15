05f13eee0186acc3d1a362ce0fd615810efdca99
ElkinAndrey <andreyridbir@gmail.com>
2022-12-15 19:42:13 +0500

    Вывод информации о приложении на странице

--- EulerJakumo/Controllers/HomeController.cs
+++ EulerJakumo/Controllers/HomeController.cs
@@ -1,12 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EulerJakumo.Data;
+using EulerJakumo.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EulerJakumo.Controllers
 {
     public class HomeController : Controller
     {
+        /// <summary>
+        /// Репозиторий приложения
+        /// </summary>
+        private IApplicationRepository applicationRepository;
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="actionResult">Репозиторий приложения</param>
+        public HomeController(IApplicationRepository actionResult)
+        {
+            this.applicationRepository = actionResult;
+        }
+
+        /// <summary>
+        /// Страница "О проекте". Запускается по умолчанию
+        /// </summary>
+        /// <returns>Страница "Home/Index"</returns>
         public IActionResult Index()
         {
-            return View((object)"!dlrow olleH");
+            List<TextDesign> aboutProductText = applicationRepository.AboutProductText;
+            return View(aboutProductText);
         }
     }
 }
