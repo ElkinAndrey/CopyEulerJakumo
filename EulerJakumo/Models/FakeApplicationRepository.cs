102f4e62b4f7df8965d48e7e94e8cb7b1c35a3a8
ElkinAndrey <andreyridbir@gmail.com>
2022-12-15 19:35:15 +0500

    Создание фейкового репозитория и фейковой базы данных, чтобы при разработке не обращаться к настоящей базе данных

---
+++ EulerJakumo/Models/FakeApplicationRepository.cs
@@ -0,0 +1,21 @@
+﻿using EulerJakumo.Data;
+
+namespace EulerJakumo.Models
+{
+    /// <summary>
+    /// Фейковый репозиторий для работы с базой данных. Нужен, чтобы не работать с базой данных при разработке.
+    /// </summary>
+    public class FakeApplicationRepository : IApplicationRepository
+    {
+        /// <summary>
+        /// Получить текст на странице "О продукте" в виде списка
+        /// </summary>
+        public List<TextDesign> AboutProductText
+        {
+            get
+            {
+                return FakeDataBase.AboutProductText;
+            }
+        }
+    }
+}
---
+++ EulerJakumo/Models/FakeDataBase.cs
@@ -0,0 +1,27 @@
+﻿using EulerJakumo.Data;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using System.Collections.Generic;
+using System.Security.Principal;
+
+namespace EulerJakumo.Models
+{
+    static public class FakeDataBase
+    {
+        static public List<TextDesign> AboutProductText { get; set; } = new List<TextDesign>()
+        {
+            new TextDesign() { TextStyle = TextStyle.Title,     Text = "О проекте" },
+            new TextDesign() { TextStyle = TextStyle.SubTitle,  Text = "Что такое Проект \"Эйлер\"?" },
+            new TextDesign() { TextStyle = TextStyle.Paragraph, Text = "Проект \"Эйлер\" — это набор интригующих задач по математике и программированию, для решения которых, однако, недостаточно одной только математической интуиции. Разумеется, математика поможет прийти к красивому и элегантному решению, но для успешного решения большинства задач без навыков программирования не обойтись."},
+            new TextDesign() { TextStyle = TextStyle.Paragraph, Text = "Основная мотивация для создания и поддержки проекта - предоставить пытливым умам платформу для погружения в незнакомые области и добавить немного веселья в процесс изучения новых идей."},
+            new TextDesign() { TextStyle = TextStyle.LineBreak, Text = "" },
+            new TextDesign() { TextStyle = TextStyle.SubTitle,  Text = "Для кого предназначены задачи?" },
+            new TextDesign() { TextStyle = TextStyle.Paragraph, Text = "Целевая аудитория проекта включает в себя студентов, которым мало университетского курса, не-математиков, которым, тем не менее, интересна математика, а также профессионалов, которым хотят быть в хорошей математической форме." },
+            new TextDesign() { TextStyle = TextStyle.LineBreak, Text = "" },
+            new TextDesign() { TextStyle = TextStyle.SubTitle,  Text = "Значит, задачи может решить кто угодно?" },
+            new TextDesign() { TextStyle = TextStyle.Paragraph, Text = "Задачи эти разной степени сложности, и большинство их предполагает индуктивное обучение. То есть очередная решённая задача открывает нечто новое, что позволит подобраться к ранее недоступной задаче. Таким образом упорный участник проекта будет медленно, но верно продвигаться по списку задач." },
+            new TextDesign() { TextStyle = TextStyle.LineBreak, Text = "" },
+            new TextDesign() { TextStyle = TextStyle.SubTitle,  Text = "..." },
+
+        };
+    }
+}
