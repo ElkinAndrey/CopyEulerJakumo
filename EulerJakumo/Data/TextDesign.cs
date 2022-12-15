2d39c29e7d5ddbead19bc8ed092949b42f4d0e08
ElkinAndrey <andreyridbir@gmail.com>
2022-12-15 17:22:11 +0500

    Создал класс для вывода текста с разными стилями

---
+++ EulerJakumo/Data/TextDesign.cs
@@ -0,0 +1,29 @@
+﻿namespace EulerJakumo.Data
+{
+    /// <summary>
+    /// Стиль текста
+    /// </summary>
+    public enum TextStyle
+    {
+        Title,      // Стиль для заголовка страницы
+        SubTitle,   // Стиль для подзаголовка
+        Paragraph,  // Параграф
+        LineBreak,  // Перенос строки
+    }
+
+    /// <summary>
+    /// Класс для вывода текста с разными стилями
+    /// /// </summary>
+    public class TextDesign
+    {
+        /// <summary>
+        /// Стиль текста
+        /// </summary>
+        public TextStyle? TextStyle { get; set; }
+
+        /// <summary>
+        /// Текст
+        /// </summary>
+        public string? Text { get; set; }
+    }
+}
