aaef6e71e3b3daa65d997bd0b608250acda8a340
ElkinAndrey <andreyridbir@gmail.com>
2022-12-17 23:36:17 +0500

    Добавление вью модели, содержащей количество страниц и список с задачами

---
+++ EulerJakumo/Models/ViewModels/Home/ProblemsPageViewModel.cs
@@ -0,0 +1,27 @@
+﻿using EulerJakumo.Data;
+
+namespace EulerJakumo.Models.ViewModels.Home
+{
+    public class ProblemsPageViewModel
+    {
+        /// <summary>
+        /// Список задач на странице
+        /// </summary>
+        public List<Problem> Problems { get; set; }
+
+        /// <summary>
+        /// Количество страниц
+        /// </summary>
+        public int AmountPages { get; set; }
+
+        /// <summary>
+        /// Количество задач
+        /// </summary>
+        public int ProblemsLength { get; set; }
+
+        /// <summary>
+        /// То, какая сейчас страница
+        /// </summary>
+        public int NowPage { get; set; }
+    }
+}
