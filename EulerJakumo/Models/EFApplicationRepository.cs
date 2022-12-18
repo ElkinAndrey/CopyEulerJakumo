96acfc15f64330e1217647fc9f3b4eb96ebee711
ElkinAndrey <andreyridbir@gmail.com>
2022-12-18 22:10:14 +0500

    Добавил класс для работы с базой данных

---
+++ EulerJakumo/Models/EFApplicationRepository.cs
@@ -0,0 +1,37 @@
+﻿using EulerJakumo.Data;
+
+namespace EulerJakumo.Models
+{
+    public class EFApplicationRepository : IApplicationRepository
+    {
+        private ApplicationDbContext context;
+
+        public EFApplicationRepository(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public List<TextDesign> AboutProductText => throw new NotImplementedException();
+
+        public List<TextDesign> FeedbackText => throw new NotImplementedException();
+
+        public List<Problem> Problems => throw new NotImplementedException();
+
+        public int ProblemsLength => throw new NotImplementedException();
+
+        public void AddProblem(Problem problem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Problem> PartProblems(int startIndex, int length)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Problem? ProblemByNumber(int number)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
