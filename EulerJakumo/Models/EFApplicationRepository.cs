bfad263de72f72b2eb5f07e2d6cd0bf5b75b5287
ElkinAndrey <andreyridbir@gmail.com>
2022-12-18 22:42:33 +0500

    Добавил возможность получения среза списка задач из реальной базы данных

--- EulerJakumo/Models/EFApplicationRepository.cs
+++ EulerJakumo/Models/EFApplicationRepository.cs
@@ -32,9 +32,36 @@ namespace EulerJakumo.Models
             context.SaveChanges();
         }
 
+        /// <summary>
+        /// Получить часть задачь из базы данных, чтобы не перегружать сервер выводом всех задач одновременно.
+        /// При попытке взять с конца больше элементов, чем есть (если startIndex + length выходит за границы списка),
+        /// будут возвращены элементы с startIndex, до конца. Ошибка вызвана не будет.
+        /// При указании начального индекса, имеющего значение больше или меньшеколичества задач, будет возвращаться 
+        /// пустой список.
+        /// </summary>
+        /// <param name="startIndex">Индекс, с которого будут взяты задачи</param>
+        /// <param name="length">Количество взятых задач</param>
+        /// <returns>Задачи</returns>
         public List<Problem> PartProblems(int startIndex, int length)
         {
-            throw new NotImplementedException();
+            int count = context.Problems.Count(); // Количество задач
+            if (startIndex >= count || startIndex < 0)
+                return new List<Problem>();
+
+            List<Problem> result;
+
+            if (startIndex + length > count)
+                result = context.Problems
+                    .Skip(startIndex)
+                    .Take(count - startIndex)
+                    .ToList();
+            else
+                result = context.Problems
+                    .Skip(startIndex)
+                    .Take(length)
+                    .ToList();
+
+            return result;
         }
 
         public Problem? ProblemByNumber(int number)
